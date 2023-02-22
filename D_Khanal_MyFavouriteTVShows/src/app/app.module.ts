436013401712e6ce4e226e67dcf11b24ea8e013d
Diya Khanal <khanal.diya5@gmail.com>
2023-02-21 21:51:17 -0500

    Assignment3
    Commited again

--- D_Khanal_MyFavouriteTVShows/src/app/app.module.ts
+++ D_Khanal_MyFavouriteTVShows/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+<<<<<<< Updated upstream
+=======
+import { ContentListComponent } from './content-list/content-list.component';
+import { FilterPipe } from './filter.pipe';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     ContentCardComponent
+=======
+    ContentCardComponent,
+    ContentListComponent,
+    FilterPipe
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
