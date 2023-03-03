a30e747796cc41d4b9286f5878d161a9469a5204
Diya Khanal <khanal.diya5@gmail.com>
2023-03-03 17:24:44 -0500

    Assignment 4
    Completed

--- D_Khanal_MyFavouriteTVShows/src/app/app.module.ts
+++ D_Khanal_MyFavouriteTVShows/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+<<<<<<< Updated upstream
+=======
+
+import { ContentListComponent } from './content-list/content-list.component';
+import { FilterPipe } from './filter.pipe';
+import { HoverAffectDirective } from './hover-affect.directive';
+
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     ContentCardComponent
+=======
+    ContentCardComponent,
+    ContentListComponent,
+    FilterPipe,
+    HoverAffectDirective
+
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
